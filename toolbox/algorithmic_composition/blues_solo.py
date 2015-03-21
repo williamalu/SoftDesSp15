668fd5b27a3ef7d935597bd4b8bd2955f580e2c8
William Lu <william.a.lu@gmail.com>
2015-03-21 16:32:26 -0400

    finished algorithmic composition toolbox

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -7,7 +7,7 @@ from random import choice
 def add_note(out, instr, key_num, duration, bpm, volume):
     """ Adds a note from the given instrument to the specified stream
 
-        out: the stream to add the note to
+        out: the stream to add the note tempo
         instr: the instrument that should play the note
         key_num: the piano key number (A 440Hzz is 49)
         duration: the duration of the note in beats
@@ -31,6 +31,42 @@ solo = AudioStream(sampling_rate, 1)
 blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
 beats_per_minute = 45				# Let's make a slow blues solo
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+curr_note = 0
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, 2.0)
 
-solo >> "blues_solo.wav"
+licks = [[[1,0.5], [1,0.5], [1,0.5], [1,0.5]],
+        [[-1,0.5], [-1,0.5], [-1,0.5], [-1,0.5]],
+        [[-1,0.5], [1,0.5], [-1,0.5], [1,0.5]],
+        [[1,0.5], [-1,0.5], [1,0.5], [-1,0.5]],
+        [[-1,0.5*1.1], [-1,0.5*0.9], [-1,0.5*1.1], [-1,0.5*0.9]],
+        [[-1,0.5*0.9], [-1,0.5*1.1], [-1,0.5*0.9], [-1,0.5*1.1]]
+        ]
+for i in range(50):
+    lick = choice(licks)
+
+    for note in lick:
+        curr_note += note[0]
+
+        if curr_note < 0:
+            curr_note += curr_note * -1
+        if curr_note > len(blues_scale) - 1:
+            curr_note += (len(blues_scale) - curr_note) * -1
+
+        print curr_note
+
+        add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, 2.0)
+
+#solo >> "blues_solo.wav"
+
+backing_track = AudioStream(sampling_rate, 1)
+Wavefile.read('backing.wav', backing_track)
+
+m = Mixer()
+
+solo *= 0.4             # adjust relative volumes to taste
+backing_track *= 2.0
+
+m.add(2.25, 0, solo)    # delay the solo to match up with backing track    
+m.add(0, 0, backing_track)
+
+m.getStream(500.0) >> "slow_blues.wav"
